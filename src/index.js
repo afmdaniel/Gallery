4a45356a1b7283989c620337c9e96ab6ddd8e66d
afmdanielwin <danielfarias380@gmail.com>
2020-05-02 12:48:30 -0300

    import dependecies and js files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 // import SASS files
-import './scss/index.scss'
+import './scss/index.scss';
+
+// Dependencies
+import 'jquery';
+import 'bootstrap';
+
+// My JS files
+import './js/core/includes'
